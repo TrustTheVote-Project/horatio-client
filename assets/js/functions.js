baaad3104921ed7c7f7892ab67151b333f305bba
Zak Greene <zak@zakgreene.com>
2015-06-15 13:07:38 -0400

    print form results as JSON to page

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -10,11 +10,32 @@
 
 // remap jQuery to $
 (function($){
+	
+	$.fn.serializeObject = function() {
+	    var o = {};
+	    var a = this.serializeArray();
+	    $.each(a, function() {
+	        if (o[this.name] !== undefined) {
+	            if (!o[this.name].push) {
+	                o[this.name] = [o[this.name]];
+	            }
+	            o[this.name].push(this.value || '');
+	        } else {
+	            o[this.name] = this.value || '';
+	        }
+	    });
+	    return o;
+	};
 
 	/* trigger when page is ready */
 	$(document).ready(function (){
-	
-		// your functions go here
+
+		$(function() {
+		    $('form').submit(function() {
+		        $('#result').text(JSON.stringify($('form').serializeObject()));
+		        return false;
+		    });
+		});
 	
 	});
 	
